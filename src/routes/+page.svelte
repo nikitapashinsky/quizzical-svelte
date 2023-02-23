329f8a5a73c60004c3a06c332c2f1d173cbc3f1b
Nikita <nikitapashinsky@gmail.com>
2023-02-23 12:30:16 +0300

    fetch and render data from API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script lang="ts">
+	import he from 'he';
+	async function getData() {
+		const res = await fetch(`https://opentdb.com/api.php?amount=10`);
+		const data = await res.json();
+		console.log(data);
+
+		if (res.ok) {
+			return data.results;
+		} else {
+			throw new Error(data);
+		}
+	}
+	let promise = getData();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await promise then questions}
+	<ul>
+		{#each questions as question}
+			<li>{he.decode(question.question)}</li>
+		{/each}
+	</ul>
+{:catch error}
+	<p>{error.message}</p>
+{/await}
